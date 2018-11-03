135d5a562f52faf968087d4ff555ff323b80ee28
Rena <renan_ftb@hotmail.com>
2018-11-03 00:17:28 -0300

    no message

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html >
+<html>
+	
+	<?php include('php/head.php'); 	?>
+
+	<body>
+		<img src="img/logoTop.png">
+		<div class="logar">
+			<form action="php/login.php" method="POST">
+			
+				
+				<table class="logar">	
+					<tr>
+						<td>Login:</td>
+						<td><input type="text" name="login"></td>
+					</tr>
+					<tr>
+						<td>Senha:</td>
+						<td><input type="password" name="senha" id="senha"></td>
+					</tr>
+						<td colspan="2"><button class="botao" type="submit" name="logar">Login</button></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+			
+		<div id="tela"></div>
+		<div id="texto">
+			<a target="_blank" href="">Sobre &emsp;</a>
+			<a target="_blank" href="">Termos de uso </a>				
+		</div>
+		
+	</body>
+</html>
+
---
+++ php/conexao.php
@@ -0,0 +1,9 @@
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$base = "u595814654_top";
+	$link = mysqli_connect($host, $user, $pass, $base);
+
+	
+?>
